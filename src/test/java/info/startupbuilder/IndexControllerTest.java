231178b462b9f2e7a680933cbd121c609c53fb77
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-03-27 23:20:08 +0800

    test controller

---
+++ src/test/java/info/startupbuilder/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package info.startupbuilder;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+public class IndexControllerTest {
+
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    public void testIndex() {
+        HttpRequest<String> request = HttpRequest.GET("/");
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hello World", body);
+    }
+}
