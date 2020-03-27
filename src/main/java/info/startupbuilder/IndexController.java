5ab61e6e89743d8831ae9ed25548906410a55f9f
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-03-27 21:10:29 +0800

    create a sample controller

---
+++ src/main/java/info/startupbuilder/IndexController.java
@@ -0,0 +1,16 @@
+package info.startupbuilder;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller
+public class IndexController {
+
+    @Get
+    @Produces(MediaType.TEXT_PLAIN)
+    public String index() {
+        return "Hello World";
+    }
+}
