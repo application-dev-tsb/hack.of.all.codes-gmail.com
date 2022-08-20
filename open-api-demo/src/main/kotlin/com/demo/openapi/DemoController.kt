c7428f246ff2d72b3a5540ed4cc4e0bbd0f17d1c
LM Bibera <hack.of.all.codes@gmail.com>
2022-08-20 09:30:53 +0800

    add a new endpoint to trigger the workflow

--- open-api-demo/src/main/kotlin/com/demo/openapi/DemoController.kt
+++ open-api-demo/src/main/kotlin/com/demo/openapi/DemoController.kt
@@ -1,13 +1,20 @@
 package com.demo.openapi
 
-import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.swagger.v3.oas.annotations.tags.Tag
 
 
+@Tag(name = "demo")
 @Controller("/demo")
 class DemoController {
-    @Get(value = "/", produces = [MediaType.APPLICATION_JSON])
-    fun demoGet(): List<Demo> = listOf(Demo(1, "Demo 1"), Demo(2, "Demo 2"))
 
+    @Get
+    fun listAll(): List<Demo> = listOf(Demo(1, "Demo 1"), Demo(2, "Demo 2"))
+
+    @Post
+    fun create(demo: Demo): Demo {
+        return Demo(123, demo.name)
+    }
 }
